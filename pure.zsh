2fc795e8f3a21662a437bf7925c9746785c8591f
Morgan Parry <m.parry@winton.com>
2018-10-04 14:07:53 +0100

    Simplified async setup, possibly squashing weird refresh bug in the process. Removed unnecessary code.

--- pure.zsh
+++ pure.zsh
@@ -112,16 +112,6 @@ prompt_pure_accept_line() {
 prompt_pure_preexec() {
 	prompt_pure_debug_output "Handling preexec"
 
-	if [[ -n $prompt_pure_git_fetch_pattern ]]; then
-		# detect when git is performing pull/fetch (including git aliases).
-		local -H MATCH MBEGIN MEND match mbegin mend
-		if [[ $2 =~ (git|hub)\ (.*\ )?($prompt_pure_git_fetch_pattern)(\ .*)?$ ]]; then
-			# we must flush the async jobs to cancel our git fetch in order
-			# to avoid conflicts with the user issued pull / fetch.
-			async_flush_jobs 'prompt_pure'
-		fi
-	fi
-
 	# shows the current dir and executed command in the title while a process is active
 	if [[ $2 != set-tab-title* ]]; then
 		prompt_pure_set_title 'ignore-escape' "$2"
@@ -169,18 +159,20 @@ prompt_pure_preprompt_render() {
 		preprompt_parts+=('%F{242}'$(print -- "\UE73C ")$(basename $_conda)'%f')
 	fi
 
-	# Add git branch and dirty status info.
-	typeset -gA prompt_pure_vcs_info
-	if [[ -n $prompt_pure_vcs_info[branch] ]]; then
-		preprompt_parts+=("%F{088}${prompt_pure_git_dirty}%F{$git_color}"$(print -- "\uF126")' ${prompt_pure_vcs_info[branch]}%f')
-	fi
-	# Git pull/push arrows.
-	if [[ -n $prompt_pure_git_arrows ]]; then
-		preprompt_parts+=('%F{104}${prompt_pure_git_arrows}%f')
-	fi
-	# Git tag info.
-	if [[ -n prompt_pure_git_tag_and_commit ]]; then
-		preprompt_parts+=("%F{$git_color}"'${prompt_pure_git_tag_and_commit}%f')
+	if [[ $1 != precmd ]]; then
+		# Add git branch and dirty status info.
+		typeset -gA prompt_pure_vcs_info
+		if [[ -n $prompt_pure_vcs_info[branch] ]]; then
+			preprompt_parts+=("%F{$git_color}"$(print -- "\uF126")' ${prompt_pure_vcs_info[branch]}%F{088}${prompt_pure_git_dirty}%f')
+		fi
+		# Git pull/push arrows.
+		if [[ -n $prompt_pure_git_arrows ]]; then
+			preprompt_parts+=('%F{104}${prompt_pure_git_arrows}%f')
+		fi
+		# Git tag info.
+		if [[ -n prompt_pure_git_tag_and_commit ]]; then
+			preprompt_parts+=("%F{$git_color}"'${prompt_pure_git_tag_and_commit}%f')
+		fi
 	fi
 
 	# Username and machine, if applicable.
@@ -229,6 +221,8 @@ prompt_pure_preprompt_render() {
 		prompt_pure_debug_output "Redrawing prompt"
 		zle && zle .reset-prompt
 		prompt_pure_debug_output "Ran reset-prompt widget: $?"
+	else
+		prompt_pure_debug_output "Skipping reset prompt; $([[ $1 == precmd ]] && print -- 'is precmd' || print -- 'prompt unchanged')"
 	fi
 
 	typeset -g prompt_pure_last_prompt=$expanded_prompt
@@ -243,37 +237,11 @@ prompt_pure_precmd() {
 	elapsed=$(prompt_pure_check_cmd_exec_time)
 	[[ -n $elapsed ]] && print -P -- "%F{088}\\uF252 $elapsed%f"
 
+	prompt_pure_create_async_tasks
+
 	prompt_pure_set_title "restore"
 
-	# preform async git dirty check and fetch
-	prompt_pure_async_tasks
-
-	# print the preprompt
 	prompt_pure_preprompt_render "precmd"
-}
-
-prompt_pure_async_git_aliases() {
-	setopt localoptions noshwordsplit
-	local dir=$1
-	local -a gitalias pullalias
-
-	# we enter repo to get local aliases as well.
-	builtin cd -q $dir
-
-	# list all aliases and split on newline.
-	gitalias=(${(@f)"$(command git config --get-regexp "^alias\.")"})
-	for line in $gitalias; do
-		parts=(${(@)=line})           # split line on spaces
-		aliasname=${parts[1]#alias.}  # grab the name (alias.[name])
-		shift parts                   # remove aliasname
-
-		# check alias for pull or fetch (must be exact match).
-		if [[ $parts =~ ^(.*\ )?(pull|fetch)(\ .*)?$ ]]; then
-			pullalias+=($aliasname)
-		fi
-	done
-
-	print -- ${(j:|:)pullalias}  # join on pipe (for use in regex).
 }
 
 prompt_pure_async_vcs_info() {
@@ -317,57 +285,9 @@ prompt_pure_async_git_dirty() {
 }
 
 prompt_pure_debug_output() {
-	#echo -e "$(command date +"%Y-%m-%dT%H:%M:%S") $1" >>~/my_prompt.log
+	#local timestamp="${$(command date +'%Y-%m-%dT%H:%M:%S.%N')[0,23]}" # Truncate to microseconds
+	#print -- "$timestamp $1" >>~/my_prompt.log
 }
-
-#prompt_pure_is_bluetooth_network_connection() {
-#	services=$(networksetup -listnetworkserviceorder | grep 'Hardware Port')
-#
-#	while read line; do
-#		sname=$(echo $line | awk -F  "(, )|(: )|[)]" '{print $2}')
-#		sdev=$(echo $line | awk -F  "(, )|(: )|[)]" '{print $4}')
-#		if [ -n "$sdev" ]; then
-#			ifout="$(ifconfig $sdev 2>/dev/null)"
-#			echo "$ifout" | grep 'status: active' > /dev/null 2>&1
-#			rc="$?"
-#			if [ "$rc" -eq 0 ]; then
-#				currentservice="$sname"
-#				currentdevice="$sdev"
-#				currentmac=$(echo "$ifout" | awk '/ether/{print $2}')
-#			fi
-#		fi
-#	done <<< "$(echo "$services")"
-#
-#	prompt_pure_debug_output "Current service is ${currentservice:-?}"
-#	if [ -n "$currentservice" ]; then
-#		if [[ $currentservice == *Bluetooth* ]]; then
-#			return 0
-#		fi
-#	fi
-#	return 1
-#}
-#
-#prompt_pure_async_git_fetch() {
-#	setopt localoptions noshwordsplit
-#	# use cd -q to avoid side effects of changing directory, e.g. chpwd hooks
-#	builtin cd -q $1
-#
-#	if prompt_pure_is_bluetooth_network_connection; then
-#		prompt_pure_debug_output "Skipping fetch; on bluetooth"
-#		return 99
-#	fi
-#	prompt_pure_debug_output "Allowing fetch; not on bluetooth"
-#
-#	# set GIT_TERMINAL_PROMPT=0 to disable auth prompting for git fetch (git 2.3+)
-#	export GIT_TERMINAL_PROMPT=0
-#	# set ssh BachMode to disable all interactive ssh password prompting
-#	export GIT_SSH_COMMAND=${GIT_SSH_COMMAND:-"ssh -o BatchMode=yes"}
-#
-#	command git -c gc.auto=0 fetch &>/dev/null || return 99
-#
-#	# check arrow status after a successful git fetch
-#	prompt_pure_async_git_arrows $1
-#}
 
 prompt_pure_async_git_arrows() {
 	setopt localoptions noshwordsplit
@@ -380,82 +300,58 @@ prompt_pure_async_git_tag_and_commit() {
 
 	builtin cd -q $1
 
-	local tag=$(command git describe --tags --exact-match HEAD 2>/dev/null)
+	local tag commit
+
+	tag=$(command git describe --tags --exact-match HEAD 2>/dev/null)
 	if [[ -n "${tag}" ]] ; then
 		tag="\uF02B ${tag} "
 	fi
 
-	print -- "${tag}\uE729 $(command git rev-parse --short=8 HEAD 2>/dev/null)"
+	commit=$(command git rev-parse --short=8 HEAD 2>/dev/null) || return $?
+	print -- "${tag}\uE729 $commit"
+	return 0
 }
 
-prompt_pure_reset_async_state() {
-	reset_all=${1:-false}
-
-	unset prompt_pure_git_dirty
-	unset prompt_pure_git_arrows
-	unset prompt_pure_git_tag_and_commit
-	prompt_pure_vcs_info[branch]=
-	prompt_pure_vcs_info[top]=
-
-	if [[ $reset_all == true ]]; then
-		unset prompt_pure_git_last_dirty_check_timestamp
-		unset prompt_pure_git_fetch_pattern
-	fi
-}
-
-prompt_pure_async_tasks() {
+prompt_pure_create_async_tasks() {
 	setopt localoptions noshwordsplit
 
-	# initialize async worker
-	((!${prompt_pure_async_init:-0})) && {
+	# lazily initialize async worker
+	(( !${prompt_pure_async_init:-0} )) && {
 		async_start_worker "prompt_pure" -u -n
 		async_register_callback "prompt_pure" prompt_pure_async_callback
 		typeset -g prompt_pure_async_init=1
 	}
 
-	typeset -gA prompt_pure_vcs_info
+	async_flush_jobs "prompt_pure"
+	typeset -ig prompt_pure_num_pending_git_jobs=0
 
-	local -H MATCH MBEGIN MEND
-	if ! [[ $PWD = ${prompt_pure_vcs_info[pwd]}* ]]; then
-		async_flush_jobs "prompt_pure"
-		prompt_pure_reset_async_state true
-	fi
-	unset MATCH MBEGIN MEND
+	typeset -gA prompt_pure_vcs_info
+	prompt_pure_vcs_info[branch]=
+	prompt_pure_vcs_info[top]=
+	unset prompt_pure_git_last_dirty_check_timestamp
+	unset prompt_pure_last_prompt
+
+	integer -l num_pending_jobs
 
 	async_job "prompt_pure" prompt_pure_async_vcs_info $PWD
-
-	# # only perform tasks inside git working tree
-	[[ -n $prompt_pure_vcs_info[top] ]] || return
-
-	prompt_pure_async_refresh
-}
-
-prompt_pure_async_refresh() {
-	setopt localoptions noshwordsplit
-
-	#if [[ -z $prompt_pure_git_fetch_pattern ]]; then
-	#	# we set the pattern here to avoid redoing the pattern check until the
-	#	# working three has changed. pull and fetch are always valid patterns.
-	#	typeset -g prompt_pure_git_fetch_pattern="pull|fetch"
-	#	async_job "prompt_pure" prompt_pure_async_git_aliases $working_tree
-	#fi
+	(( num_pending_jobs++ ))
 
 	async_job "prompt_pure" prompt_pure_async_git_arrows $PWD
-	async_job "prompt_pure" prompt_pure_async_git_tag_and_commit $PWD
-
-	## do not preform git fetch if it is disabled or working_tree == HOME
-	#if (( ${PURE_GIT_PULL:-1} )) && [[ $working_tree != $HOME ]]; then
-	#	# tell worker to do a git fetch
-	#	async_job "prompt_pure" prompt_pure_async_git_fetch $PWD
-	#fi
+	(( num_pending_jobs++ ))
 
 	# if dirty checking is sufficiently fast, tell worker to check it again, or wait for timeout
 	integer time_since_last_dirty_check=$(( EPOCHSECONDS - ${prompt_pure_git_last_dirty_check_timestamp:-0} ))
 	if (( time_since_last_dirty_check > ${PURE_GIT_DELAY_DIRTY_CHECK:-1800} )); then
 		unset prompt_pure_git_last_dirty_check_timestamp
-		# check check if there is anything to pull
+		# check if there is anything to pull
 		async_job "prompt_pure" prompt_pure_async_git_dirty ${PURE_GIT_UNTRACKED_DIRTY:-1} $PWD
+		(( num_pending_jobs++ ))
 	fi
+
+	async_job "prompt_pure" prompt_pure_async_git_tag_and_commit $PWD
+	(( num_pending_jobs++ ))
+
+	typeset -ig prompt_pure_num_pending_git_jobs=num_pending_jobs
 }
 
 prompt_pure_print_all_colours() {
@@ -478,13 +374,15 @@ prompt_pure_check_git_arrows() {
 
 prompt_pure_async_callback() {
 	setopt localoptions noshwordsplit
-	local job=$1 code=$2 output=$3 exec_time=$4 next_pending=$6
-	local do_render=0
+	local job=$1 code=$2 output=$3 exec_time=$4
+	local pending_git_job_complete=0
 
 	case $job in
 		prompt_pure_async_vcs_info)
 			local -A info
 			typeset -gA prompt_pure_vcs_info
+
+			prompt_pure_debug_output "Got new vcs info"
 
 			# parse output (z) and unquote as array (Q@)
 			info=("${(Q@)${(z)output}}")
@@ -502,76 +400,64 @@ prompt_pure_async_callback() {
 			fi
 			unset MATCH MBEGIN MEND
 
-			# update has a git toplevel set which means we just entered a new
-			# git directory, run the async refresh tasks
-			[[ -n $info[top] ]] && [[ -z $prompt_pure_vcs_info[top] ]] && prompt_pure_reset_async_state && prompt_pure_async_refresh
-
 			# always update branch and toplevel
 			prompt_pure_vcs_info[branch]=$info[branch]
 			prompt_pure_vcs_info[top]=$info[top]
 
-			do_render=1
+			pending_git_job_complete=1
 			;;
-		prompt_pure_async_git_aliases)
-			if [[ -n $output ]]; then
-				# append custom git aliases to the predefined ones.
-				prompt_pure_git_fetch_pattern+="|$output"
-			fi
-			;;
+
 		prompt_pure_async_git_dirty)
-			local prev_dirty=$prompt_pure_git_dirty
+			prompt_pure_debug_output "Got new dirty status"
 			if (( code == 0 )); then
 				unset prompt_pure_git_dirty
 			else
-				typeset -g prompt_pure_git_dirty="* "
+				typeset -g prompt_pure_git_dirty=" *"
 			fi
-
-			[[ $prev_dirty != $prompt_pure_git_dirty ]] && do_render=1
 
 			# When prompt_pure_git_last_dirty_check_timestamp is set, the git info is displayed in a different color.
 			# To distinguish between a "fresh" and a "cached" result, the preprompt is rendered before setting this
 			# variable. Thus, only upon next rendering of the preprompt will the result appear in a different color.
 			(( $exec_time > 5 )) && prompt_pure_git_last_dirty_check_timestamp=$EPOCHSECONDS
+
+			pending_git_job_complete=1
 			;;
-		prompt_pure_async_git_fetch|prompt_pure_async_git_arrows)
-			# prompt_pure_async_git_fetch executes prompt_pure_async_git_arrows
-			# after a successful fetch.
+
+		prompt_pure_async_git_arrows)
 			if (( code == 0 )); then
+				prompt_pure_debug_output "Got new git arrow status"
 				local REPLY
 				prompt_pure_check_git_arrows ${(ps:\t:)output}
 				if [[ $prompt_pure_git_arrows != $REPLY ]]; then
-					prompt_pure_debug_output "New git arrows: $REPLY"
 					typeset -g prompt_pure_git_arrows=$REPLY
-					do_render=1
 				fi
-			elif (( code != 99 )); then
-				# Unless the exit code is 99, prompt_pure_async_git_arrows
-				# failed with a non-zero exit status, meaning there is no
-				# upstream configured.
+			else
 				prompt_pure_debug_output "Git arrows failed with $code"
 				if [[ -n $prompt_pure_git_arrows ]]; then
 					unset prompt_pure_git_arrows
-					do_render=1
 				fi
 			fi
+
+			pending_git_job_complete=1
 			;;
+
 		prompt_pure_async_git_tag_and_commit)
 			if (( code == 0 )); then
 				typeset -g prompt_pure_git_tag_and_commit=$output
+				prompt_pure_debug_output "Received new git tag/commit info: $prompt_pure_git_tag_and_commit"
 			else
+				prompt_pure_debug_output "Failed to retrieve git tag/commit info: $code"
 				unset prompt_pure_git_tag_and_commit
 			fi
-			do_render=1
+
+			pending_git_job_complete=1
 			;;
 	esac
 
-	if (( next_pending )); then
-		(( do_render )) && typeset -g prompt_pure_async_render_requested=1
-		return
+	if (( pending_git_job_complete )); then
+		(( prompt_pure_num_pending_git_jobs-- ))
+		(( prompt_pure_num_pending_git_jobs == 0 )) && prompt_pure_preprompt_render "async"
 	fi
-
-	[[ ${prompt_pure_async_render_requested:-$do_render} = 1 ]] && prompt_pure_preprompt_render "async"
-	unset prompt_pure_async_render_requested
 }
 
 prompt_pure_setup() {
